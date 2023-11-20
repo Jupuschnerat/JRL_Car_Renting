ea09548e326ec04b329f86c0bd80084eb9123304
Julia Puschnerat <jupuschnerat@gmail.com>
2023-11-20 19:15:47 -0300

    bookings model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,8 @@
 class Car < ApplicationRecord
   has_many :users
 
+  validates :plate, uniqueness: true, lenght: {is: 7}, presence: true
+  validates :model, presence: true
+  validates :city, presence: true
+  validates :price, presence: true
 end
