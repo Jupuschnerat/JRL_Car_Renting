e80f471a313c47a397c2f041304c94c654e340de
Julia Puschnerat <jupuschnerat@gmail.com>
2023-11-21 10:41:29 -0300

    cars contollers done

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,36 +1,51 @@
 class CarsController < ApplicationController
-  def index
+  before_action :set_car, only: %i[ show edit update destroy ]
+
+  # GET /cars
+   def index
     @cars = Car.all
   end
 
+  # GET /cars/1
   def show
-    @car = Car.find(params[:id])
   end
 
+  # GET /cars/new
   def new
     @car = Car.new
   end
 
+  # POST /cars
   def create
     @car = Car.new(car_params)
     if @car.save
-      redirect_to
+      redirect_to cars_path(@flat)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  # GET /cars/1/edit
   def edit
   end
 
+  # PATCH/PUT /cars/1
   def update
+    @car.update(car_params)
+    redirect_to cars_path(@cars)
   end
 
+  # DELETE /cars/1
   def destroy
+    @car.destroy
+    redirect_to cars_path
   end
 
   private
 
+  def set_car
+    @car = Car.find(params[:id])
+  end
   def car_params
     params.require(:car).permit(:plate, :model, :city, :price)
   end
