5cdc876c5c52eb919bfe736e8fc87b458093e66d
Julia Puschnerat <jupuschnerat@gmail.com>
2023-11-21 14:02:07 -0300

    seeding

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
-  skip_after_action :verify_authorized, only: :index
-  skip_after_action :verify_policy_scoped, only: :index
+  # skip_after_action :verify_authorized, only: :index
+  # skip_after_action :verify_policy_scoped, only: :index
 
   def index
     @user = current_user
