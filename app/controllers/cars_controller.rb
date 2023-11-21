a416c52c397b1be9f0cc8ab37bee80e0edc7e751
Julia Puschnerat <jupuschnerat@gmail.com>
2023-11-21 10:33:15 -0300

    some of cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,37 @@
 class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:plate, :model, :city, :price)
+  end
 end
