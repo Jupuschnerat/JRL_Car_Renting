d7fde234f055e092d90ab8ec49601e51b391f8af
Julia Puschnerat <jupuschnerat@gmail.com>
2023-11-21 13:20:21 -0300

    edit models

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,12 @@
 class Car < ApplicationRecord
-  has_many :users
+
+  geocoded_by :city
+
+  belongs_to :user
+  has_many :bookings
 
   validates :plate, uniqueness: true, lenght: {is: 7}, presence: true
-  validates :model, presence: true
-  validates :city, presence: true
-  validates :price, presence: true
+  validates :model, :city, :price, :photo, presence: true
+
+  after_validation :geocode, if: :will_save_change_to_address?
 end
