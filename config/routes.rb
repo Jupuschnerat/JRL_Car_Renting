9d87816a8868d9b1fad3a0be1ad8479c95b35c9b
Julia Puschnerat <jupuschnerat@gmail.com>
2023-11-21 13:45:00 -0300

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  root to: 'pages#home'
 
-  resources :cars do
-    resources :bookings
+  get 'dashboard', to: 'dashboard#index'
+  resources :profiles, only: :update
+
+  resources :boats do
+    resources :bookings, only: [ :new, :create ]
   end
-  # Defines the root path route ("/")
-  # root "posts#index"
+
+  resources :bookings, only: [ :show ]
 end
