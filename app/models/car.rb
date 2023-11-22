b59f3ed60ab93de4d073f10c628fe37a72d785ee
lucasprado <lucaspradobr@gmail.com>
2023-11-22 14:58:36 -0300

    active storage setup

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,6 +4,7 @@ class Car < ApplicationRecord
 
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
 
   # validates :plate, uniqueness: true, lenght: {is: 7}, presence: true
   validates :model, :city, :price, :photo, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_121549) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_153604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_121549) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -80,4 +82,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_121549) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cars", "users"
 end
