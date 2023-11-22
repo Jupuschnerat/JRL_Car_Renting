2ced8e6410bb7c0a93c1eb3480b7534c5b5684ec
Rodrigo Ramos <rodrigo.sr@hotmail.com>
2023-11-22 15:08:12 -0300

    form and homepage

--- db/schema.rb
+++ db/schema.rb
@@ -38,14 +38,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_145130) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
-    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birth_date"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
