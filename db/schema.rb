fc44ce437d9d3e137205a38220be6d5d4b0f872b
Julia Puschnerat <jupuschnerat@gmail.com>
2023-11-23 10:11:56 -0300

    newcontrollers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_145130) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_124104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_145130) do
     t.float "longitude"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "users", force: :cascade do |t|
