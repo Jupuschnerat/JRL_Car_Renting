a7f0062de48cda057f63640d8356f1bc939c1dff
Julia Puschnerat <jupuschnerat@gmail.com>
2023-11-23 14:00:17 -0300

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
+  get 'pages/home'
+  resources :cars do
+    resources :bookings
+  end
+
+  # Additional routes for user's bookings
+  resources :bookings, only: [:edit, :update, :destroy]
 
   root to: 'pages#home'
-  resources :profiles, only: :update
-  resources :cars, only: [:edit, :update, :new, :create, :destroy] do
-    resources :bookings, only: [:create, :new]
-  end
-  resources :cars, only: [:index, :show]
-  resources :bookings, only: [:edit, :update, :destroy]
+
   get 'my_cars', to: 'cars#my_cars'
   get 'my_cars/:id', to: 'cars#my_cars_show', as: :my_car
 end
