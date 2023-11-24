822996043edbfbd683d8bbb6b995d65715ce3a7b
lucasprado <lucaspradobr@gmail.com>
2023-11-24 11:42:26 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,39 +16,33 @@ User.delete_all
 
 puts 'Seed: Seeding...'
 
+first_name = ['Julia', 'Rodrigo', 'Lucas', 'Andre', 'Carla', 'Luca', 'Stella', 'Chloé', 'Milene', 'Tais']
+last_name = ['Puschnerat', 'Ramos', 'Prado', 'Menezes', 'Valdivia', 'Severo', 'Mascaro', 'Geerdens', 'Cardoso', 'Amorim' ]
+
+
 10.times do |i|
   User.create!(
-    email: "Paulo#{i}@gmail.com",
-    first_name: "Paulo#{i}",
-    last_name: "Silva",
+    email: "#{first_name[i]}@gmail.com",
+    first_name: "#{first_name[i]}",
+    last_name: "#{last_name[i]}",
     birth_date: Date.parse('2023-11-30'),
     password: "123456"
   )
-  car = Car.new(
-    user: User.last,
-    plate: "IA7B8D1",
-    description: "fast car",
-    model: "Porsche",
-    city: "London",
-    price: 200.00
-  )
-  file = URI.open("https://www.jeep.com.br/content/dam/jeep/products/675/1ad/1/2024/page/hero.webp")
+  puts User.last
 
-  car.photo.attach(io: file, filename: "car.png", content_type: "image/png")
-  car.save
-
-  car1 = Car.new(
-    user: User.last,
-    plate: "IA7B8D1",
-    description: "small car for loners",
-    model: "Fusca",
-    city: "London",
-    price: 200.00
-  )
-  file = URI.open("https://www.jeep.com.br/content/dam/jeep/products/675/1ad/1/2024/page/hero.webp")
-
-  car1.photo.attach(io: file, filename: "car.png", content_type: "image/png")
-  car1.save
 end
+
+car = Car.new(
+  user: User.last,
+  plate: "IA7B8D1",
+  description: "fast car",
+  model: "Porsche",
+  city: "London",
+  price: 200.00
+)
+file = URI.open("https://www.jeep.com.br/content/dam/jeep/products/675/1ad/1/2024/page/hero.webp")
+
+car.photo.attach(io: file, filename: "car.png", content_type: "image/png")
+car.save!
 
 puts 'Seed: Created!'
